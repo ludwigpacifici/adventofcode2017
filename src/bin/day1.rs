0a8bb0f56bb4aefc91cb4330dc1a5d00aab1b4d8
Ludwig PACIFICI <ludwig@lud.cc>
2017-12-04 20:51:44 +0000

    Use cycle and skip

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,7 +1,6 @@
-#[macro_use]
 extern crate failure;
 
-use failure::{err_msg, Error};
+use failure::Error;
 use std::fs::File;
 use std::io::Read;
 use std::string::String;
@@ -18,7 +17,6 @@ fn run() -> Result<(), Error> {
     let mut file = File::open("input/day1.txt")?;
     let mut input = String::new();
     file.read_to_string(&mut input)?;
-    input.pop();
 
     println!("a: {:?}", run_a(&input));
     println!("b: {:?}", run_b(&input));
@@ -26,48 +24,28 @@ fn run() -> Result<(), Error> {
     Ok(())
 }
 
-fn run_a(input: &str) -> Result<u32, Error> {
-    let input = push_first(input)?;
-    Ok(captcha_sum_next(&input))
+fn run_a(input: &str) -> u32 {
+    captcha_sum_next(input)
 }
 
-fn run_b(input: &str) -> Result<u32, Error> {
+fn run_b(input: &str) -> u32 {
     captcha_sum_middle(input)
-}
-
-fn push_first(data: &str) -> Result<String, Error> {
-    ensure!(!data.is_empty(), "data is empty");
-
-    let first = data.chars()
-        .nth(0)
-        .ok_or_else(|| err_msg("cannot get first element"))?;
-    let mut data = data.to_owned();
-    data.push(first);
-
-    ensure!(data.len() >= 2, "could not push first char");
-    Ok(data)
 }
 
 fn captcha_sum_next(data: &str) -> u32 {
     captcha_sum(data, 1)
 }
 
-fn captcha_sum_middle(data: &str) -> Result<u32, Error> {
-    ensure!(
-        data.len() % 2 == 0,
-        "list must have an even number of elements"
-    );
-
+fn captcha_sum_middle(data: &str) -> u32 {
     let middle = data.len() / 2;
-    let half_sum = captcha_sum(data, middle);
-    Ok(2 * half_sum)
+    captcha_sum(data, middle)
 }
 
 fn captcha_sum(data: &str, shift: usize) -> u32 {
     data.chars()
-        .zip(data[shift..].chars())
-        .filter(|pair| pair.0 == pair.1)
-        .filter_map(|pair| pair.0.to_digit(10))
+        .zip(data.chars().cycle().skip(shift))
+        .filter(|&(a, b)| a == b)
+        .filter_map(|(a, _)| a.to_digit(10))
         .sum()
 }
 
@@ -77,18 +55,18 @@ mod tests {
 
     #[test]
     fn test_run_a() {
-        assert_eq!(3, run_a("1122").unwrap()); // As of today, Failure does not implement PartialEq
-        assert_eq!(4, run_a("1111").unwrap());
-        assert_eq!(0, run_a("1234").unwrap());
-        assert_eq!(9, run_a("91212129").unwrap());
+        assert_eq!(3, run_a("1122"));
+        assert_eq!(4, run_a("1111"));
+        assert_eq!(0, run_a("1234"));
+        assert_eq!(9, run_a("91212129"));
     }
 
     #[test]
     fn test_run_b() {
-        assert_eq!(6, run_b("1212").unwrap());
-        assert_eq!(0, run_b("1221").unwrap());
-        assert_eq!(4, run_b("123425").unwrap());
-        assert_eq!(12, run_b("123123").unwrap());
-        assert_eq!(4, run_b("12131415").unwrap());
+        assert_eq!(6, run_b("1212"));
+        assert_eq!(0, run_b("1221"));
+        assert_eq!(4, run_b("123425"));
+        assert_eq!(12, run_b("123123"));
+        assert_eq!(4, run_b("12131415"));
     }
 }
